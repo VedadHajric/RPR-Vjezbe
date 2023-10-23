4e5b7119057f2adc17ac618fe10a99cbfcf4ea3f
VedadHajric <vhajric1@etf.unsa.ba>
2023-10-23 16:08:24 +0200

    nedovrseni drugi zadatak

---
+++ Zadatak2/src/main/java/org/example/Osoba.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Osoba{
+    protected String ime;
+    protected String prezime;
+    public Osoba(String s1, String s2) {
+        this.ime = s1;
+        this.prezime = s2;
+    }
+    public String toString() {
+        return
+    }
+}
---
+++ Zadatak2/src/main/java/org/example/Uposlenik.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Uposlenik extends Osoba{
+    public Uposlenik(String s1, String s2) {
+        super(s1, s2);
+    }
+}
