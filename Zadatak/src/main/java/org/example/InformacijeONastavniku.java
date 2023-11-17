8fa7d4e98b1f72bd832e14b91199db64de510f68
VedadHajric <vhajric1@etf.unsa.ba>
2023-11-17 17:34:36 +0100

    Lab5

---
+++ Zadatak/src/main/java/org/example/InformacijeONastavniku.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class InformacijeONastavniku {
+    private String ime;
+    private String prezime;
+    private String titulu;
+    public String getIme() {
+        return ime;
+    }
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getTitulu() {
+        return titulu;
+    }
+
+    public void setTitulu(String titulu) {
+        this.titulu = titulu;
+    }
+}
