9da03c219f1bd8e642b0605948fc83acc4799893
VedadHajric <vhajric1@etf.unsa.ba>
2023-11-17 17:53:20 +0100

    Lab5

--- Zadatak/src/main/java/org/example/InformacijeONastavniku.java
+++ Zadatak/src/main/java/org/example/InformacijeONastavniku.java
@@ -1,28 +1,10 @@
 package org.example;
 
-public class InformacijeONastavniku {
-    private String ime;
-    private String prezime;
+public class InformacijeONastavniku extends LicneInformacije{
     private String titulu;
-    public String getIme() {
-        return ime;
-    }
-    public void setIme(String ime) {
-        this.ime = ime;
-    }
-
-    public String getPrezime() {
-        return prezime;
-    }
-
-    public void setPrezime(String prezime) {
-        this.prezime = prezime;
-    }
-
     public String getTitulu() {
         return titulu;
     }
-
     public void setTitulu(String titulu) {
         this.titulu = titulu;
     }
--- Zadatak/src/main/java/org/example/Main.java
+++ Zadatak/src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InformacijeOStudentu student = new InformacijeOStudentu();
+        student.setIme("Asmir");
+        student.setPrezime("Hamidović");
+        student.setBrojIndexa("19856");
+        student.setGodinaStudija("druga");
+        System.out.println(student.getIme() + " " + student.getPrezime() + ", " + student.getBrojIndexa()
+                + ", " + student.getGodinaStudija() + ".");
+        /*System.out.println(student.getPrezime());
+        System.out.println(student.getBrojIndexa());
+        System.out.println(student.getGodinaStudija());*/
+        InformacijeONastavniku nastavnik = new InformacijeONastavniku();
+        nastavnik.setIme("Robert");
+        nastavnik.setPrezime("Kumerle");
+        nastavnik.setTitulu("diplomirani znalac");
+        System.out.println(nastavnik.getIme() + " " + nastavnik.getPrezime() + " " + nastavnik.getTitulu() + ".");
+
     }
 }
